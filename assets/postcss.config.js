fdb01b312d864ef9c3a31271f08971a071fd6fd4
John Norman Capule <johnnormancapule@gmail.com>
2018-01-11 23:09:50 +0800

    Reconfig brunch to webpack

---
+++ assets/postcss.config.js
@@ -0,0 +1,11 @@
+const DEBUG = process.env.NODE_ENV !== 'production'
+
+module.exports = {
+  plugins: [
+    require('postcss-import')(),
+    require('postcss-cssnext')(),
+  ].concat(DEBUG ? [
+    require('postcss-reporter')(),
+    require('postcss-browser-reporter')(),
+  ] : []),
+}
